95887b85213dccf7e31d714a509ad9849b39b0a2
orhantok143 <119065177+orhantok143@users.noreply.github.com>
2024-03-12 02:01:02 +0300

    q

--- src/redux/login/loginService.js
+++ src/redux/login/loginService.js
@@ -7,7 +7,14 @@ import { headers } from '../baseUrl';
 
 
 export const userLogin = async (url, data) => {
-    return axios.post(url, data, headers);
+    return axios.post(url, data, {
+        // CORS configuration settings
+        headers: {
+            'Content-Type': 'application/json', // Assuming JSON data
+            'Access-Control-Allow-Origin': 'https://flamingodb.netlify.app, https://flamingo-mn.netlify.app',
+        },
+        withCredentials: true, // Send credentials (if applicable)
+    });
 };
 
 
